d304167dcc444cc570f3a3e0637abc821f0090ae
mansingc <48505940+mansingc@users.noreply.github.com>
2021-04-19 15:35:43 -0400

    Update Game.java

--- Game (2)/Game/src/game/Game.java
+++ Game (2)/Game/src/game/Game.java
@@ -10,7 +10,7 @@ import java.util.ArrayList;
 
 /**
  *
- * @author Kubra Bas Arshdeep Dhakla Christopher Mansingh
+ * @author Kubra Bas Arshdeep Dhalla Christopher Mansingh
  */
 public abstract class Game {
     
